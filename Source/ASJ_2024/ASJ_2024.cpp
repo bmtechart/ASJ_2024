00049799674f0e5867f24f5028748b7e70bf9471
Brian Murphy <brianmurphy@bmtechart.com>
2024-11-08 21:33:27 -0700

    Initial commit

---
+++ Source/ASJ_2024/ASJ_2024.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ASJ_2024.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ASJ_2024, "ASJ_2024" );
